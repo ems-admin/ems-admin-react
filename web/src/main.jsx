af5c093e36224df163af1643eea55f4b927b07c0
starao520 <starao520@outlook.com>
2025-10-30 22:15:16 +0800

    无用代码清理

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,14 +2,8 @@ import React from 'react'
 import {BrowserRouter} from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
-// import { Provider } from 'react-redux'
-// import store from "@/store/store.js";
-// import {PersistGate} from 'redux-persist/integration/react'
-// import {persistStore} from "redux-persist";
 import {ConfigProvider} from "antd";
 import '@/assets/css/Common.css'
-
-// let persistor = persistStore(store);
 
 const validateMessages = {
     required: "${label}不能为空",
